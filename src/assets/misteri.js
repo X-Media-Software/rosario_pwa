35f05aabeb06a788ad76ab150267ead48b22bda6
Antonio Fortino <thoniorf@gmail.com>
2019-05-05 18:59:24 +0200

    Added real comments

---
+++ src/assets/misteri.js
@@ -0,0 +1,27 @@
+export const Dolorosi = [
+  {
+    title: "Contempliamo Gesù nell'agonia nel Getsemani",
+    comment:
+      "Piangi GESÙ nel Getsemani perché è dura la Passione che ti si presenta. Ma piu’ forte è il dolore per le anime che , seppur redente dal tuo sangue si perderanno, per le anime che vivono nella tiepidezza, per color che non sono testimoni veri del tuo Vangelo. Ed io , Gesù, come vivo il mio cristianesimo? Fa che una tua lacrima scenda sul mio cuore per renderlo docile alla tua Parola."
+  },
+  {
+    title: "Contempliamo Gesù che viene flagellato",
+    comment:
+      "Nel pretorio, vieni flagellato GESÙ. In silenzio mi avvicino e mi inginocchio vicino a te. Ti chiedo perdono GESÙ perché è anche per me che ti lasci flagellare. Nella tua mente ci sono gli uomini del presente, del passato e del futuro. Ti prego, ti supplico, accogli e consacra nel tuo Sangue divino tutte le anime affinché nessuna sia più preda del demonio."
+  },
+  {
+    title: "Contempliamo Gesù incoronato di spine",
+    comment:
+      "GESÙ, dopo averti flagellato i soldati continuano la loro opera di degradazione. Tu, ti lasci fare e ti lasci dire perché è solo così che si abbatte la superbia umana. GESÙ aiutaci a guardare te quando i nostri fratelli ci mancano di carità, ci offendono e ci umiliano. Il tuo sguardo mite metta in silenzio il nostro orgoglio e l’amor proprio."
+  },
+  {
+    title: "Contempliamo Gesù mentre precorre la via del Calvario",
+    comment:
+      "Gesù, mi metto accanto a Maria tua Madre che silenziosa segue i tuoi passi faticosi. Nel suo sguardo ci sei solo tu, il suo Figlio e Figlio di Dio. Gesù, quando la tua croce mi sembra più pesante, fa che veda Maria, tua e mia Madre, che mi segue con amore. Fa che senta la sua mano che mi rialza nel momento della caduta e mi aiuta a vivere con fede la sofferenza del momento."
+  },
+  {
+    title: "Contempliamo Gesù che muore in croce",
+    comment:
+      "La Croce è innalzata ed io con Maria tua e mia Madre, mi inginocchio per starti vicino Gesù e per imparare da lei, la donna forte, come si vive il dolore, nella fede e nella speranza. Gesù grazie per il sacrificio di te stesso per noi e fa che la tua morte oggi doni ad ogni uomo la rinascita della vita dello spirito."
+  }
+];
