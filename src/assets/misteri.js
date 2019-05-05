e6d2fba6c23684393e71ae0238cfb69814b524db
Antonio Fortino <thoniorf@gmail.com>
2019-05-06 00:19:30 +0200

    Added new mysteries

--- src/assets/misteri.js
+++ src/assets/misteri.js
@@ -25,3 +25,84 @@ export const Dolorosi = [
       "La Croce è innalzata ed io con Maria tua e mia Madre, mi inginocchio per starti vicino Gesù e per imparare da lei, la donna forte, come si vive il dolore, nella fede e nella speranza. Gesù grazie per il sacrificio di te stesso per noi e fa che la tua morte oggi doni ad ogni uomo la rinascita della vita dello spirito."
   }
 ];
+export const Gioiosi = [
+  {
+    title: "L’angelo del signore porto’ l’annuncio a maria",
+    comment:
+      "Ti guardo Maria mentre l’Angelo si avvicina a Te e ti annuncia che diventerai Madre. Penso ai tanti giovani che, persi nel chiasso del mondo e della vana gioia, non odono più la voce soave dello Spirito. Quante vocazioni perdute, quanti cammini di santità persi. Aiutaci Maria a far silenzio in noi, a far tacere le voci della superbia e dell’orgoglio per assaporare la pace dell’anima."
+  },
+  {
+    title: "Maria visita la cugina elisabetta\n",
+    comment:
+      "Ti guardo Maria, mentre dimentica di Te, ti rechi a compiere un servizio alla cugina Elisabetta. Perdonaci quando siamo egoisti, quando stanchi ci lamentiamo del lavoro che ci attende  rispondendo  e donando con impazienza a chi ci chiede qualcosa. Ricordaci Madre che ogni battezzato è sacerdote e il lavoro delle sue mani offerto sull’altare di ogni Chiesa dove si celebra la S. Messa diventa offerta unita al sacrificio di Cristo per i bisogni dell’umanità."
+  },
+  {
+    title: "Gesù nasce a betlemme",
+    comment:
+      "Vergine benedetta, vengo con il pensiero nella stalla di Betlem e mi inginocchio innanzi alla “culla”. Con commozione lo prendo tra le mie braccia e contemplo il Dio fatto Bambino. A Lui affido tutti i bambini del mondo perché possano avere dei veri genitori ad immagine tua, Maria, e di S. Giuseppe. E a voi, Santa Famiglia, chiedo la grazia di aiutare ogni uomo a tornare bambino nel cuore quando ancora non conoscevano il giudizio, la critica e la mormorazione per vedere il mondo e le cose con occhi e cuore puro."
+  },
+  {
+    title: "Gesù e’ presentato al tempio",
+    comment:
+      "Fedele alla legge, portate il bambino al tempio. Sacra Famiglia, insegnate voi all’intera umanità la fedeltà alla legge di Dio, con Gesù offrite anche il nostro cuore e la nostra mente affinché purificati dal suo Sangue Preziosissimo, ogni uomo possa abbracciare i comandamenti e vivere nel suo amore."
+  },
+  {
+    title: "Gesù dodicenne e’ ritrovato nel tempio",
+    comment:
+      "Con quanta trepidazione avete cercato Gesù. Ma, trovatolo, una sola domanda, Maria, è uscita dal tuo cuore: “figlio perché ci hai fato questo?”. Madre porto a Te tutti i perché degli uomini del nostro tempo. Le domande senza risposta di ogni cuore. “Perché questa sofferenza, perché questa malattia, questa morte improvvisa? Perché…perché?”. Gesù, Giuseppe e Maria aiutateci ad avere fede e a baciare i divini voleri di Dio, a volte incomprensibili ma sempre pieni di amore e di un futuro ricco di speranza."
+  }
+];
+export const Gloriosi = [
+  {
+    title: "Gesù risorge dai morti",
+    comment:
+      "Hai lasciato il sepolcro e sei risorto Gesù, donandoci la tua pace e la figliolanza divina. Ti chiediamo di alimentare i nostri cuori con lo spirito di preghiera per rimanere in comunione con Te, con il Padre e lo Spirito Santo e, nei momenti di difficoltà, fa che con la grazia che ci viene dal Battesimo impariamo a vivere e testimoniare la gioia cristiana come uomini e donne veramente risorti in Te Signore."
+  },
+  {
+    title: "Contempliamo Gesù che viene flagellato",
+    comment:
+      "Con gli uomini di Galilea anche noi ti guardiamo salire Cielo e come vorremmo trattenerti! Gesù noi sappiamo che sei andato a prepararci un posto. Fa che in quel giorno, quando Tu mi chiamerai, io sia pronto a iniziare il santo viaggio per poterti vedere, adorare e ringraziare. E ti ricordiamo tanti fratelli che, persi nelle cose effimere del mondo, non pensano alla gioia dell’eternità e rischiano di non entrare in quel posto che hai preparato per loro in Paradiso."
+  },
+  {
+    title: "Lo spirito santo discende sugli apostoli con maria nel cenacolo",
+    comment:
+      "Mi unisco agli Apostoli e Maria e con loro ti invoco, Spirito Santo. Donaci di non offenderti con le parole e il disamore perché tu mai abbia ad allontanarti per un comportamento poco evangelico. Vieni Spirito Santo, rinnova le nostre famiglie e la faccia della terra."
+  },
+  {
+    title: "Maria è assunta in cielo in anima e corpo",
+    comment:
+      "Madre, in quel giorno che solo il Padre conosce, anch’io riceverò l’ultima chiamata e allora inizierà per me il viaggio senza ritorno, il viaggio dell’ingresso in Patria. Donami di prepararmi ogni giorno aggiungendo atti, virtù e opere nella “mia valigia” per essere associato alla tua pace, alla tua gioia e beatitudine con tutti i miei cari."
+  },
+  {
+    title: "Maria è incoronata regina",
+    comment:
+      "Un giorno dicesti nel Magnificat: “Hai guardato all’umiltà della tua serva e tutte le generazioni mi chiameranno beata”. Insegnami Madre il vero cammino d’umiltà per essere un giorno esaltato e premiato nel Regno eterno dove sarò finalmente e per sempre."
+  }
+];
+export const Luminosi = [
+  {
+    title: "Gesù è battezzato nel giordano",
+    comment:
+      "Ti guardo Gesù mentre il Divino Spirito si posa su di Te. Che cosa grande è il Battesimo! Ti preghiamo di battezzare con il nostro desiderio tutti i bambini non nati o appartenenti ad altre religioni, affinché lo Spirito Santo possa portare i suoi frutti di conversione e beatitudine in ogni cuore e fa che doni la sua luce a chi con ignoranza chiede di essere “sbattezzato”."
+  },
+  {
+    title: "Gesù compie il primo miracolo a cana",
+    comment:
+      "Maria, tu puoi tutto. A te ci rivolgiamo offrendoti tutti i desideri di bene del nostro cuore e di ogni uomo presente sulla faccia della terra. Tu, che ci conosci, donaci quello che ci manca per essere dei veri cristiani. La tua fede ha strappato il primo miracolo di Gesù per degli sposi. Aumenta la nostra fede perché anche tutta l’acqua delle nostre giornate possa diventare quel vino che l’Eucaristia trasforma nel Corpo e nel Sangue di Gesù."
+  },
+  {
+    title: "Gesù annuncia la conversione e il regno di dio",
+    comment:
+      "Dall’inizio della Chiesa risuona l’annuncio del Regno, la chiamata alla conversione, i beni eterni che ci attendono. Ma il mondo, ieri come oggi, che cosa scolta e segue? Noi Gesù cosa cerchiamo? Donaci di camminare come pellegrini coscienti che un giorno tutto dovremo lasciare per entrare in quel Regno che possederemo in  eterno e per sempre."
+  },
+  {
+    title: "Gesù si trasfigura sul monte tabor",
+    comment:
+      "Gesù, entriamo anche noi nella nube luminosa dove sei con Mosè ed Elia. E’ così bello rimanere con voi e lasciare il mondo. Rafforza la nostra fede perché nei momenti bui della nostra quotidianità possiamo alzare lo sguardo al cielo e ritrovare la speranza per non cedere alla tristezza, al peccato o alla disperazione."
+  },
+  {
+    title: "Gesù istituisce il sacramento dell’eucaristia.",
+    comment:
+      "Quale Dio è come te Signore che vieni Bambino in mezzo a noi e prendi su di te la fatica e il lavoro dell’uomo. Nell’Ultima Cena dopo il segno di estrema umiltà della lavanda dei piedi, ti consegni a noi donando te stesso come cibo e bevanda. Dio d’amore donami un cuore esente da malizia e maldicenza per accoglierti con purezza d’amore e ottenere da te tutto ciò che ti domando per me, la mia famiglia, il mondo intero."
+  }
+];
